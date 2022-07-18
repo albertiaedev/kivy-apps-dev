d1bf2c34410efe59ad975f792eae88cb238c87c3
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-07-18 10:24:12 -0400

    kivyAppTest.
    This app is merely a test that asks you for a single-line or multi-line input that will show on the screen after you press the 'Print it!' button.

--- kivyAppTest/kivyAppTest.py
+++ kivyAppTest/kivyAppTest.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from.kivy.uix.label import Label
+from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
